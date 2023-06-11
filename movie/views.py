aafc9c4d69e9a61a9badfcda3ba9555f633ee211
LodyginArtyom <articrafter123@mail.ru>
2023-06-12 00:47:05 +0300

    MovieView 2

--- movie/views.py
+++ movie/views.py
@@ -7,5 +7,5 @@ class MovieView(View):
 
     def get(self, request):
         movies = Movie.objects.all()
-        return render(request, "movies/movie_list.html", {'movie_list': movies})
+        return render(request, "movies/movies.html", {'movie_list': movies})
 # Create your views here.
